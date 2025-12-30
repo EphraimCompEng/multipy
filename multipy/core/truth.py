d6b9f45a9d95dee1b5f1369deb95660c80fd6f44
EphraimCompEng <madugba.finance@proton.me>
2025-12-30 01:17:00 +0000

    Update truth.py

--- multipy/core/truth.py
+++ multipy/core/truth.py
@@ -15,9 +15,19 @@ simplification, etc., before applying multiprocessing and beyond.
 
 
 
+from _typeshed import GenericPath
 import multipy as mp
+import pathlib
 import itertools
 
 
-def truth():
-    ...
+def truth_scope(domain: tuple, range: tuple=(0,-1)) -> combinations:
+    """
+    Generate a generator based on the domain and range of a desired truth table
+    >>> domain = (min_input, max_input)
+    >>> range  = (min_output, max_output)
+
+    Use '-1' for no upper bound
+    """
+
+    scope =
