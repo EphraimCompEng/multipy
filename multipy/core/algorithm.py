77faca7bc30f7f1f129a820042a5f608f96958e1
EphraimCompEng <madugba.finance@proton.me>
2025-12-31 17:37:57 +0000

    Update algorithm.py

--- multipy/core/algorithm.py
+++ multipy/core/algorithm.py
@@ -1,27 +1,11 @@
 #########################################
 # Algorithm Are Defined Using Templates #
 #########################################
-"""
-An algorithm is the application of multiple templates until no partial
-products are left.
-
-Algorithm objects must collect any number of templates based on:
-    - bitwidth
-    - template composition -- MultiPy should not stop users making any
-      algorithm, even if suboptimal
-    - Saturation -- Set to original bit width
-    (Unsure if users likely to need saturation to arbitrary bitwidths)
-
-Applying simple templates can be hardcoded, however complex templates
-need to be analysed before execution. Especially so when implementing
-decoders, flooding and sneaky tricks like using carry-in(cin) on adders.
 
 
-"""
 
-
-from typing import Any
 import multipy as mp
+from typing import Any
 
 class Algorithm(mp.Matrix):
     """
