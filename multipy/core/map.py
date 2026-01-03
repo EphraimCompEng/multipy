655ad504e549946fcb5a8567bd90dc64d825b164
EphraimCompEng <madugba.finance@proton.me>
2026-01-03 19:06:15 +0000

    Update map.py

--- multipy/core/map.py
+++ multipy/core/map.py
@@ -19,14 +19,14 @@ def build_map(
     A map matrix holds signed hexadecimal numbers representing a vertical
     offset for a given bit when applied to an algorithm.
 
-    [map]\t   [matrix]\t[effect]\n
-    00 02 01\t _ 0 1\t  _ 1 _\n
-    00 FF 00\t 0 1 _\t  0 _ 1\n
-    00 00 00\t _ _ _\t  _ 0 _\n
+    [matrix] || [map---] || [effect]
+     _ 0 1 > || 00 02 01 || > _ 1 _
+     0 1 _ > || 00 FF 00 || > 0 _ 1
+     _ _ _ > || 00 00 00 || > _ 0 _
     """
     ...
 
-def dadda_map():
+def build_dadda_map():
     """
     Return map to collect partial products into an upside down triangle:
     a prerequisite for the dadda algorithm.
